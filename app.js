6c537b19c972f2c1f09aa17c06ce43c901ea0fe1
MuhammadShahbaz47 <azizterai32@gmail.com>
2020-08-09 19:52:24 +0500

    QUIZ-APP

--- app.js
+++ app.js
@@ -34,6 +34,90 @@ function q3Red(){
     }
 }
 
+function q4Green(){
+    document.getElementById("q4Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q4Red(){
+    document.getElementById("q4Green").style.backgroundColor="lightgreen";
+    var q4Red=document.getElementsByClassName("q4Red")
+    for (let i = 0; i < q4Red.length; i++) {
+        q4Red[i].style.backgroundColor="tomato";
+    }
+}
+
+function q5Green(){
+    document.getElementById("q5Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q5Red(){
+    document.getElementById("q5Green").style.backgroundColor="lightgreen";
+    var q5Red=document.getElementsByClassName("q5Red")
+    for (let i = 0; i < q5Red.length; i++) {
+        q5Red[i].style.backgroundColor="tomato";
+    }
+}
+
+function q6Green(){
+    document.getElementById("q6Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q6Red(){
+    document.getElementById("q6Green").style.backgroundColor="lightgreen";
+    var q6Red=document.getElementsByClassName("q6Red")
+    for (let i = 0; i < q6Red.length; i++) {
+        q6Red[i].style.backgroundColor="tomato";
+    }
+}
+
+function q7Green(){
+    document.getElementById("q7Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q7Red(){
+    document.getElementById("q7Green").style.backgroundColor="lightgreen";
+    var q7Red=document.getElementsByClassName("q7Red")
+    for (let i = 0; i < q7Red.length; i++) {
+        q7Red[i].style.backgroundColor="tomato";
+    }
+}
+
+function q8Green(){
+    document.getElementById("q8Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q8Red(){
+    document.getElementById("q8Green").style.backgroundColor="lightgreen";
+    var q8Red=document.getElementsByClassName("q8Red")
+    for (let i = 0; i < q8Red.length; i++) {
+        q8Red[i].style.backgroundColor="tomato";
+    }
+}
+
+function q9Green(){
+    document.getElementById("q9Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q9Red(){
+    document.getElementById("q9Green").style.backgroundColor="lightgreen";
+    var q9Red=document.getElementsByClassName("q9Red")
+    for (let i = 0; i < q9Red.length; i++) {
+        q9Red[i].style.backgroundColor="tomato";
+    }
+}
+
+function q10Green(){
+    document.getElementById("q10Green").style.backgroundColor="lightgreen";
+    score++;
+}
+function q10Red(){
+    document.getElementById("q10Green").style.backgroundColor="lightgreen";
+    var q10Red=document.getElementsByClassName("q10Red")
+    for (let i = 0; i < q10Red.length; i++) {
+        q10Red[i].style.backgroundColor="tomato";
+    }
+}
+
 function check(){
     alert("Your Total Score is "+score)
 }
